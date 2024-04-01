9ba1f5ef05cc53040901e89570f9a9ea3d1dd1d5
PCC <105041457+pcchristie@users.noreply.github.com>
2024-04-01 21:00:46 +0800

    Fix header not being white due to unused Styles import being removed.

--- src/components/headerbanner/headerbanner.js
+++ src/components/headerbanner/headerbanner.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from './headerbanner.module.css'
 
 function HeaderBanner(props) {
     return (
         <div className={props.className}>
-            <h1>The Playlist Maker</h1>
+            <h1 className={styles.h1}>The Playlist Maker</h1>
         </div>
     )
 }
